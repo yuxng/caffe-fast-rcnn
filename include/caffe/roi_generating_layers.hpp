3e6cee4ff7e53a7b4e8e20217aaf2679866f78da
Yu Xiang <xyfudan@gmail.com>
2015-09-01 17:49:36 -0700

    start working on roi generating layer

---
+++ include/caffe/roi_generating_layers.hpp
@@ -0,0 +1,85 @@
+// ------------------------------------------------------------------
+// Subcategory-CNN
+// Copyright (c) 2015 CVGL Stanford
+// Licensed under The MIT License
+// Written by Yu Xiang
+// ------------------------------------------------------------------
+
+#ifndef CAFFE_ROI_GENERATING_LAYERS_HPP_
+#define CAFFE_ROI_GENERATING_LAYERS_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/loss_layers.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+/* ROIGeneratingLayer - Region of Interest Generating Layer
+*/
+template <typename Dtype>
+class ROIGeneratingLayer : public Layer<Dtype> {
+ public:
+  explicit ROIGeneratingLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "ROIGenerating"; }
+
+  virtual inline int MinBottomBlobs() const { return 10; }
+  virtual inline int MaxBottomBlobs() const { return 10; }
+  virtual inline int MinTopBlobs() const { return 6; }
+  virtual inline int MaxTopBlobs() const { return 6; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+};
+
+/* ROITestingLayer - Region of Interest Testing Layer
+*/
+template <typename Dtype>
+class ROITestingLayer : public Layer<Dtype> {
+ public:
+  explicit ROITestingLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "ROITesting"; }
+
+  virtual inline int MinBottomBlobs() const { return 2; }
+  virtual inline int MaxBottomBlobs() const { return 2; }
+  virtual inline int MinTopBlobs() const { return 2; }
+  virtual inline int MaxTopBlobs() const { return 2; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_ROI_GENERATING_LAYERS_HPP_
